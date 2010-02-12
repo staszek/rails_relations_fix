9972229348f89c9880eda9fa0dae797a4b5e59f4
Stanisław Kolarzowski <stanislaw.kolarzowski@gmail.com>
2010-02-12 13:56:15 +0100

    Add test helper and association test

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,43 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require "mocha"
+require "action_controller"
+require 'active_resource'
+require 'active_record'
+require 'active_support'
+require "active_record/base"
+require "active_record/associations"
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rails_relations_fix'
 
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+ActiveRecord::Base.configurations = true
+ 
+ActiveRecord::Schema.verbose = false
+ActiveRecord::Schema.define(:version => 1) do
+  create_table :users do |t|
+    t.string :name
+    t.integer :movies_count
+  end
+  
+  create_table :movies do |t|
+    t.string :name
+    t.string :subject_type
+    t.integer :subject_id
+  end
+
+end
+
 class Test::Unit::TestCase
+  
+  class User < ActiveRecord::Base
+    has_many :movies, :as => :subject
+  end
+  
+  class Movie < ActiveRecord::Base
+    belongs_to :subject, :polymorphic => true, :counter_cache => true
+  end
 end
--- test/test_rails_relations_fix.rb
+++ test/test_rails_relations_fix.rb
@@ -1,7 +1,20 @@
 require 'helper'
 
-class TestRailsRelationsFix < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class TestRailsRelationFix< Test::Unit::TestCase
+  context "RailsRelationFix tests" do
+    setup do
+      @user = User.create(:name => "John Rambo") 
+      @titanic = @user.movies.create(:name => "Titanic")
+    end
+
+    context "Models testing" do
+      should "User has movie" do
+        assert_equal(@titanic, @user.movies.first)
+      end 
+    
+      should "Movie belongs to user" do
+        assert_equal(@user, @titanic.subject)
+      end 
+    end
   end
 end
